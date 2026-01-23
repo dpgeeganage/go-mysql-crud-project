83c5042fec8e63f81387c2422f0160bffc971653
gedalk <dasun.geeganage@ifs.com>
2026-01-23 16:23:10 +0530

    add validation for model

--- model/user.go
+++ model/user.go
@@ -2,6 +2,6 @@ package model
 
 type User struct {
 	ID int `json:"id"`
-	Name string `json:"name"`
-	Email string `json:"email"`
+	Name string `json:"name" binding:"required,min=2,max=30,alpha"`
+	Email string `json:"email" binding:"required,email"`
 }
