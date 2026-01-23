18879c74713bab76d5f7ece25f5a5b9b1c143b9f
gedalk <dasun.geeganage@ifs.com>
2026-01-23 15:11:34 +0530

    add .env file for DB connection string

--- config/database.go
+++ config/database.go
@@ -4,13 +4,29 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	_"github.com/go-sql-driver/mysql"
+	"os"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var DB *sql.DB
 
 func ConnectDB() {
-	dsn := "root:root123@tcp(127.0.0.1:3306)/go_crud"
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("error loading .env file")
+	}
+
+	// Read values from variables
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	// Data Source Name
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
 
 	// Open the database connection
 	db, err := sql.Open("mysql", dsn)
