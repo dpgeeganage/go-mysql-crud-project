5438429289bf47277f9935056774715ede188f5a
gedalk <dasun.geeganage@ifs.com>
2026-01-24 22:55:49 +0530

    improved main file

--- main.go
+++ main.go
@@ -4,16 +4,18 @@ import (
 	"go-mysql-crud-project/config"
 	"go-mysql-crud-project/routes"
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 func main() {
 	// Connect to the database
 	config.ConnectDB()
+	defer config.DB.Close()
 
 	router := gin.Default()
 	routes.SetupRoutes(router)
 
-	router.Run(":8080")
+	router.Run(":" + os.Getenv("SERVER_PORT"))
 }
 
 // http://localhost:8080/users
